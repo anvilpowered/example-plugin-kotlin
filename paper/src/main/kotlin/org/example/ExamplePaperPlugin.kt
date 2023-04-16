901905942724ab1887cfd61f73a63c15e0283b30
Alexander Städing <alexanderstaeding@gmail.com>
2023-04-17 01:35:31 +0200

    Fix style again (sigh)

--- paper/src/main/kotlin/org/example/ExamplePaperPlugin.kt
+++ paper/src/main/kotlin/org/example/ExamplePaperPlugin.kt
@@ -3,7 +3,6 @@ package org.example
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
-
 class ExamplePaperPlugin : JavaPlugin(), Listener {
     override fun onEnable() {
         // WTF?
